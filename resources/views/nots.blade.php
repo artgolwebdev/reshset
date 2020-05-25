0e0f83103e219586ea2ba1f0a35c2fc6e54bbf09
EskimoLemon <artyombb@gmail.com>
2020-05-25 10:41:30 +0300

    m

--- resources/views/nots.blade.php
+++ resources/views/nots.blade.php
@@ -4,7 +4,7 @@
 
 <div class="container">
     <div class="row">
-        <div class="col-md-10 col-md-offset-1">
+        <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
                 <div class="panel-heading text-center">Notifications</div>
                 <div class="panel-body">
